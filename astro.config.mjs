42f3c7ee3c194f185a1ba1c734995916f9ddb9b5
Cubone7 <98514655+Cubone7@users.noreply.github.com>
2024-01-17 15:00:18 +0800

    island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kun-website.netlify.app/"
+  site: "https://kun-website.netlify.app/",
+  integrations: [preact()]
 });
